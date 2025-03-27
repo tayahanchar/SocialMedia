c9249b7a626505f8bca6433a9be37dbf788a0842
Taya Hanchar <t.hanchar@andersenlab.com>
2025-03-27 12:20:26 +0200

    init: init project

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  SafeAreaView,
+} from 'react-native';
+
+function App() {
+
+
+  return (
+    <SafeAreaView></SafeAreaView>
+  );
+}
+
+
+export default App;
